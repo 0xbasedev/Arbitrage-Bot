224c522f135e065d8328cdb9dd49b3a062d84e4a
0xBaseDev <krynetixss@gmail.com>
2025-12-01 04:07:34 -0500

    Refactor UniswapFlashQuery with new interfaces and license
    Updated contract to use SPDX license identifier and modified interfaces for Uniswap V2.

--- contracts/UniswapFlashQuery.sol
+++ contracts/UniswapFlashQuery.sol
@@ -1,22 +1,28 @@
-//SPDX-License-Identifier: UNLICENSED 
-pragma solidity 0.6.12; 
-
+// SPDX-License-Identifier: MIT
+// Edited by msg.sender
+pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
+/// @title Uniswap V2 Pair interface
 interface IUniswapV2Pair {
     function token0() external view returns (address);
     function token1() external view returns (address);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
 }
 
-abstract contract UniswapV2Factory  {
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
-    function allPairsLength() external view virtual returns (uint);
+/// @title Uniswap V2 Factory interface
+interface IUniswapV2Factory {
+    function allPairsLength() external view returns (uint);
+    function allPairs(uint) external view returns (address);
+    function getPair(address tokenA, address tokenB) external view returns (address);
 }
 
-// In order to quickly load up data from Uniswap-like market, this contract allows easy iteration with a single eth_call
+/// @title FlashBotsUniswapQuery
+/// @notice Enables batch querying of Uniswap V2 pairs and reserves for analytics, bots, and monitoring
 contract FlashBotsUniswapQuery {
+    /// @notice Batch retrieve reserves data for multiple pairs
+    /// @param _pairs Array of Uniswap V2 pairs
+    /// @return Array of [reserve0, reserve1, blockTimestampLast] for each pair as uint256[3]
     function getReservesByPairs(IUniswapV2Pair[] calldata _pairs) external view returns (uint256[3][] memory) {
         uint256[3][] memory result = new uint256[3][](_pairs.length);
         for (uint i = 0; i < _pairs.length; i++) {
@@ -25,7 +31,12 @@ contract FlashBotsUniswapQuery {
         return result;
     }
 
-    function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
+    /// @notice Batch retrieve pair token addresses and contract addresses by range
+    /// @param _uniswapFactory Uniswap V2 factory contract address
+    /// @param _start Start index (inclusive)
+    /// @param _stop Stop index (exclusive)
+    /// @return Array of [token0, token1, pairAddress] for each pair as address[3]
+    function getPairsByIndexRange(IUniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
         uint256 _allPairsLength = _uniswapFactory.allPairsLength();
         if (_stop > _allPairsLength) {
             _stop = _allPairsLength;
