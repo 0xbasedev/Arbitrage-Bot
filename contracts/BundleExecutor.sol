fccc30f8ed9ad9e3ff0cf372412cdd6f91a777fb
0xBaseDev <krynetixss@gmail.com>
2025-12-01 04:58:21 -0500

    Update FlashBotsMultiCall to use WETH address parameter
    Refactor FlashBotsMultiCall to accept WETH address in constructor and modify related functions.

--- contracts/BundleExecutor.sol
+++ contracts/BundleExecutor.sol
@@ -1,34 +1,21 @@
 // SPDX-License-Identifier: MIT
-// edited by msg.sender
 pragma solidity ^0.8.30;
 
 interface IERC20 {
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
-    function allowance(address owner, address spender) external view returns (uint);
-
     function approve(address spender, uint value) external returns (bool);
     function transfer(address to, uint value) external returns (bool);
     function transferFrom(address from, address to, uint value) external returns (bool);
+    function balanceOf(address owner) external view returns (uint);
 }
-
 interface IWETH is IERC20 {
     function deposit() external payable;
     function withdraw(uint) external;
 }
 
-/// @title FlashBots MultiCall Executor
-/// @notice Allows multicalls and secure execution for WETH-based arbitrage strategies
 contract FlashBotsMultiCall {
     address private immutable owner;
     address private immutable executor;
-    IWETH private constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IWETH public immutable WETH;
 
     modifier onlyExecutor() {
         require(msg.sender == executor, "Not executor");
@@ -40,9 +27,10 @@ contract FlashBotsMultiCall {
         _;
     }
 
-    constructor(address _executor) payable {
+    constructor(address _executor, address _weth) payable {
         owner = msg.sender;
         executor = _executor;
+        WETH = IWETH(_weth);
         if (msg.value > 0) {
             WETH.deposit{value: msg.value}();
         }
@@ -50,25 +38,52 @@ contract FlashBotsMultiCall {
 
     receive() external payable {}
 
-    /// @notice Batch call multiple targets with WETH balance accounting
-    /// @param _wethAmountToFirstMarket Amount of WETH to send to first market
-    /// @param _ethAmountToCoinbase ETH reward for miner (coinbase)
-    /// @param _targets Target contract addresses to call
-    /// @param _payloads Calldata for each target
+    /**
+     * Optionally, you can add a flashloan entrypoint here in future
+     * Take care to use correct params and callback for flashloan provider.
+     */
+
+    /// @notice Batch call DEXes
     function uniswapWeth(
         uint256 _wethAmountToFirstMarket,
         uint256 _ethAmountToCoinbase,
         address[] calldata _targets,
         bytes[] calldata _payloads
-    )
-        external
-        onlyExecutor
-        payable
-    {
+    ) external onlyExecutor payable {
         require(_targets.length == _payloads.length, "Targets/payloads mismatch");
         uint256 _wethBalanceBefore = WETH.balanceOf(address(this));
         WETH.transfer(_targets[0], _wethAmountToFirstMarket);
 
+        for (uint256 i = 0; i < _targets.length; i++) {
+            (bool _success, ) = _targets[i].call(_payloads[i]);
+            require(_success, "Call failed");
+        }
+
+        uint256 _wethBalanceAfter = WETH.balanceOf(address(this));
+        require(_wethBalanceAfter > _wethBalanceBefore + _ethAmountToCoinbase, "Balance check failed");
+
+        if (_ethAmountToCoinbase == 0) return;
+
+        uint256 _ethBalance = address(this).balance;
+        if (_ethBalance < _ethAmountToCoinbase) {
+            WETH.withdraw(_ethAmountToCoinbase - _ethBalance);
+        }
+        (bool sent, ) = block.coinbase.call{value: _ethAmountToCoinbase}("");
+        require(sent, "Coinbase payment failed");
+    }
+
+    /// @notice Owner utility - proxy call
+    function call(
+        address payable _to,
+        uint256 _value,
+        bytes calldata _data
+    ) external onlyOwner payable returns (bytes memory) {
+        require(_to != address(0), "Bad target");
+        (bool _success, bytes memory _result) = _to.call{value: _value}(_data);
+        require(_success, "Call failed");
+        return _result;
+    }
+}
         for (uint256 i = 0; i < _targets.length; i++) {
             (bool _success, bytes memory _response) = _targets[i].call(_payloads[i]);
             require(_success, "Call failed");
