33930d4a747dfb11c179c7fcc6b75ea3c878ac13
0xBaseDev <krynetixss@gmail.com>
2025-12-01 05:08:54 -0500

    Refactor tests to use WNATIVE_ADDRESS instead of WETH_ADDRESS

--- test/Arbitrage.test.ts
+++ test/Arbitrage.test.ts
@@ -1,5 +1,5 @@
 import { Arbitrage, getBestCrossedMarket } from "../src/Arbitrage"
-import { WETH_ADDRESS} from "../src/addresses"
+import { WNATIVE_ADDRESS } from "../src/addresses"
 import { UniswappyV2EthPair } from "../src/UniswappyV2EthPair";
 import { BigNumber } from "ethers";
 import { ETHER } from "../src/utils";
@@ -8,20 +8,19 @@ const MARKET_ADDRESS = "0x0000000000000000000000000000000000000001"
 const TOKEN_ADDRESS = "0x000000000000000000000000000000000000000a"
 const PROTOCOL_NAME = "TEST";
 
-
 describe('Arbitrage', function() {
-  let groupedWethMarkets: Array<UniswappyV2EthPair>
+  let groupedWnativeMarkets: Array<UniswappyV2EthPair>
   beforeEach(() => {
-    groupedWethMarkets = [
-      new UniswappyV2EthPair(MARKET_ADDRESS, [TOKEN_ADDRESS, WETH_ADDRESS], PROTOCOL_NAME),
-      new UniswappyV2EthPair(MARKET_ADDRESS, [TOKEN_ADDRESS, WETH_ADDRESS], PROTOCOL_NAME),
+    groupedWnativeMarkets = [
+      new UniswappyV2EthPair(MARKET_ADDRESS, [TOKEN_ADDRESS, WNATIVE_ADDRESS], PROTOCOL_NAME),
+      new UniswappyV2EthPair(MARKET_ADDRESS, [TOKEN_ADDRESS, WNATIVE_ADDRESS], PROTOCOL_NAME),
     ]
   })
   it('Calculate Crossed Markets', function() {
-    groupedWethMarkets[0].setReservesViaOrderedBalances([ETHER, ETHER.mul(2)])
-    groupedWethMarkets[1].setReservesViaOrderedBalances([ETHER, ETHER])
+    groupedWnativeMarkets[0].setReservesViaOrderedBalances([ETHER, ETHER.mul(2)])
+    groupedWnativeMarkets[1].setReservesViaOrderedBalances([ETHER, ETHER])
 
-    const bestCrossedMarket = getBestCrossedMarket([groupedWethMarkets], TOKEN_ADDRESS);
+    const bestCrossedMarket = getBestCrossedMarket([groupedWnativeMarkets], TOKEN_ADDRESS);
     if (bestCrossedMarket === undefined) {
       fail("No crossed Market")
       return
@@ -30,10 +29,10 @@ describe('Arbitrage', function() {
     expect(bestCrossedMarket.profit).toEqual(BigNumber.from("0x012be1d487a428ce"))
   });
   it('Calculate markets that do not cross', function() {
-    groupedWethMarkets[0].setReservesViaOrderedBalances([ETHER, ETHER])
-    groupedWethMarkets[1].setReservesViaOrderedBalances([ETHER, ETHER])
+    groupedWnativeMarkets[0].setReservesViaOrderedBalances([ETHER, ETHER])
+    groupedWnativeMarkets[1].setReservesViaOrderedBalances([ETHER, ETHER])
 
-    const bestCrossedMarket = getBestCrossedMarket([groupedWethMarkets], TOKEN_ADDRESS);
+    const bestCrossedMarket = getBestCrossedMarket([groupedWnativeMarkets], TOKEN_ADDRESS);
     if (bestCrossedMarket === undefined) {
       fail("No crossed Market")
       return
