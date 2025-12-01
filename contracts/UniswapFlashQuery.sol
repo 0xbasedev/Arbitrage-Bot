208ac3f2e9d04010aaad676411bedba643512b0e
0xBaseDev <krynetixss@gmail.com>
2025-12-01 04:10:30 -0500

    Upgrade Solidity version and refactor functions

--- contracts/UniswapFlashQuery.sol
+++ contracts/UniswapFlashQuery.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Edited by msg.sender
-pragma solidity 0.6.12;
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.30;
 
 /// @title Uniswap V2 Pair interface
 interface IUniswapV2Pair {
@@ -23,10 +22,17 @@ contract FlashBotsUniswapQuery {
     /// @notice Batch retrieve reserves data for multiple pairs
     /// @param _pairs Array of Uniswap V2 pairs
     /// @return Array of [reserve0, reserve1, blockTimestampLast] for each pair as uint256[3]
-    function getReservesByPairs(IUniswapV2Pair[] calldata _pairs) external view returns (uint256[3][] memory) {
+    function getReservesByPairs(IUniswapV2Pair[] calldata _pairs)
+        external
+        view
+        returns (uint256[3][] memory)
+    {
         uint256[3][] memory result = new uint256[3][](_pairs.length);
-        for (uint i = 0; i < _pairs.length; i++) {
-            (result[i][0], result[i][1], result[i][2]) = _pairs[i].getReserves();
+        for (uint256 i = 0; i < _pairs.length; i++) {
+            (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = _pairs[i].getReserves();
+            result[i][0] = reserve0;
+            result[i][1] = reserve1;
+            result[i][2] = blockTimestampLast;
         }
         return result;
     }
@@ -36,7 +42,11 @@ contract FlashBotsUniswapQuery {
     /// @param _start Start index (inclusive)
     /// @param _stop Stop index (exclusive)
     /// @return Array of [token0, token1, pairAddress] for each pair as address[3]
-    function getPairsByIndexRange(IUniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
+    function getPairsByIndexRange(
+        IUniswapV2Factory _uniswapFactory,
+        uint256 _start,
+        uint256 _stop
+    ) external view returns (address[3][] memory) {
         uint256 _allPairsLength = _uniswapFactory.allPairsLength();
         if (_stop > _allPairsLength) {
             _stop = _allPairsLength;
@@ -44,7 +54,7 @@ contract FlashBotsUniswapQuery {
         require(_stop >= _start, "start cannot be higher than stop");
         uint256 _qty = _stop - _start;
         address[3][] memory result = new address[3][](_qty);
-        for (uint i = 0; i < _qty; i++) {
+        for (uint256 i = 0; i < _qty; i++) {
             IUniswapV2Pair _uniswapPair = IUniswapV2Pair(_uniswapFactory.allPairs(_start + i));
             result[i][0] = _uniswapPair.token0();
             result[i][1] = _uniswapPair.token1();
