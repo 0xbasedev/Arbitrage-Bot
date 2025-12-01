6347e59bf894863689996f994e729dbce263833b
0xBaseDev <krynetixss@gmail.com>
2025-12-01 04:28:25 -0500

    Refactor BundleExecutor by removing unused code
    Removed redundant code and cleaned up the BundleExecutor contract.

--- contracts/BundleExecutor.sol
+++ contracts/BundleExecutor.sol
@@ -107,14 +107,3 @@ contract FlashBotsMultiCall {
         require(_success, "Call failed");
         return _result;
     }
-}        }
-        block.coinbase.transfer(_ethAmountToCoinbase);
-    }
-
-    function call(address payable _to, uint256 _value, bytes calldata _data) external onlyOwner payable returns (bytes memory) {
-        require(_to != address(0));
-        (bool _success, bytes memory _result) = _to.call{value: _value}(_data);
-        require(_success);
-        return _result;
-    }
-}
